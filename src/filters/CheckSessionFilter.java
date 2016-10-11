9cbcc21ca810ddaad067b1b5d7112379ce3a0811
gnoomqw6 <gnoomqw6@gmail.com>
2016-10-11 14:16:31 +0300

    перенос на комп

---
+++ src/filters/CheckSessionFilter.java
@@ -0,0 +1,34 @@
+package filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "CheckingSession")
+public class CheckSessionFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+//        HttpServletRequestWrapper requestWrapper = new HttpServletRequestWrapper((HttpServletRequest) req);
+//        HttpServletResponseWrapper responseWrapper = new HttpServletResponseWrapper((HttpServletResponse) resp);
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession(false);
+        if (session == null || session.isNew()) {
+            response.sendRedirect(request.getContextPath() + "/pages/main.jsp");
+        } else {
+            chain.doFilter(req, resp);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
+
+
